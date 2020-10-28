8b5e00ec57af15ffd86dc44fc04d61945f8aefe9
SergiPicart <elpicart@gmail.com>
2020-10-28 09:54:56 +0100

    Added not only data from retrospective analysis, but all the paper

--- R/data.R
+++ R/data.R
@@ -1,17 +1,39 @@
-#' retroData: benchmark for pathway prediction with
-#' retrospective data
+#' retroData: Companion data package for the article
+#' "The effect of statistical normalization on network
+#' propagation scores", https://doi.org/10.1093/bioinformatics/btaa896.
 #'
+#' This package contains all the data that was used to
+#' run the analysis. This includes specific gene-pathway human
+#' annotations from KEGG, and other network and genetic data
+#' from public sources.
+#'
+#' Yeast analysis:
+#' The \link[igraphdata]{yeast} network.
+#'
+#' DLBCL:
+#' The interactome in \link[DLBCL]{interactome}.
+#' KEGG human pathways from 2017 in \link[retroData]{keggdlbcl}.
+#' The gene backgrounds were derived from \link[ALL]{ALL} and
+#' \link[DLBCL]{dataLym}.
+#'
+#' Prospective pathway prediction:
 #' The retroData package provides the BioGRID
 #' protein-protein interaction
-#' network from 2011 and KEGG pathways from 2011 and 2018.
-#' This can be useful for validating the performance of
+#' network from 2011 \link[retroData]{biogrid2011}
+#' and KEGG pathways from 2011 \link[retroData]{kegg2011}
+#' and 2018 \link[retroData]{kegg2018}.
+#' This was useful for validating the performance of
 #' network-based gene prediction methods in a six year
-#' horizon case.
+#' horizon.
 #'
-#' @section Data objects: the pathways can be found at
-#' \link[retroData]{kegg2011} and
-#' \link[retroData]{kegg2018}, whereas the network is
-#' contained in \link[retroData]{biogrid2011}
+#' @section Data objects: (1) the yeast interactome
+#' \link[retroData]{yeast}, (2) the KEGG pathways
+#' \link[retroData]{keggdlbcl}, the human interactome
+#' \link[retroData]{interactome} and the backgrounds
+#' \link[retroData]{ALL} and \link[retroData]{dataLym},
+#' (3) the KEGG pathways \link[retroData]{kegg2011} and
+#' \link[retroData]{kegg2018}, and the network
+#' \link[retroData]{biogrid2011}
 #'
 #' @docType package
 #' @name retroData
@@ -62,3 +84,51 @@ NULL
 #'
 #' @source The KEGG rest API \url{rest.kegg.jp}
 "kegg2018"
+
+#' KEGG pathways for the DLBCL/HPRD simulated gene expression
+#'
+#' 320 KEGG pathways from 2017, Release 83.0+/09-09, Sep 17.
+#'
+#' @format A table with 28077 rows, 2 columns, with 320 KEGG pathways
+#'
+#' @source The KEGG rest API \url{rest.kegg.jp}
+"keggdlbcl"
+
+#' Yeast network from the igraphdata package
+#'
+#' This object is a copy of \code{\link[igraphdata]{yeast}}
+#'
+#' @format An igraph object with 2617 nodes and 11855 edges, representing a
+#' yeast protein interactions, von Mering et al.
+#'
+#' @source The \code{igraphdata} R package
+"yeast"
+
+#' HPRD network from the DLBCL package
+#'
+#' This object is a copy of \code{\link[DLBCL]{interactome}}
+#'
+#' @format A graphNEL object with 9386 nodes and 36504 edges, representing a
+#' human interactome from HPRD.
+#'
+#' @source The \code{DLBCL} R package
+"interactome"
+
+#' dataLym gene expression p-values from the DLBCL package
+#'
+#' This object is a copy of \code{\link[DLBCL]{dataLym}}
+#'
+#' @format A data.frame with 3583 rows and 8 columns, with genes
+#' and p-values for differential expression.
+#'
+#' @source The \code{DLBCL} R package
+"dataLym"
+
+#' ALL data from the ALL package
+#'
+#' This object is a copy of \code{\link[ALL]{ALL}}
+#'
+#' @format ExpressionSet with 12625 features, 128 samples
+#'
+#' @source The \code{ALL} R package
+"ALL"
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# retroData: benchmark for pathway prediction with retrospective data
+# retroData: companion data package for the article "The effect of statistical normalization on network propagation scores",  https://doi.org/10.1093/bioinformatics/btaa896
 
 ## Install
 
@@ -8,22 +8,32 @@ retroData path and execute
 ```
 devtools::install()
 ```
-You can also install the package through the zip file
+You can also install the package through the built zip file
 
 ```
-R CMD INSTALL retroData_0.0.2.tar.gz
+R CMD INSTALL retroData_0.2.0.tar.gz
 ```
 
 or through its [GitHub repository](https://github.com/b2slab/retroData). 
 
-Get started by looking at the package help
+## Getting started
+
+Have a glance at the package help
 
 ```
 ?retroData
 ```
 
-and the three objects: 
+List of data objects from the three main sections in the article:
 
-* biogrid2011: PPI network from before 2011
-* kegg2011: KEGG pathways from 2011
-* kegg2018: KEGG pathways from 2018
+1. Yeast analysis: 
+  - `yeast`: network
+2. Simulated gene expression analysis: 
+  - `interactome`: network
+  - `ALL`: gene background
+  - `dataLym`: gene background
+  - `keggdlbcl`: KEGG gene sets from 2017
+3. Prospective pathway prediction:
+  - `biogrid2011` PPI network from before 2011
+  - `kegg2011`: KEGG pathways from 2011
+  - `kegg2018`: KEGG pathways from 2018
